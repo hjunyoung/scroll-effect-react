74a8d3dfd9fb3799f1d2d71da1f35e5256041b27
Junyoung Hong <hjunyoung10@gmail.com>
2021-10-14 22:20:06 +0900

    💄 Add a GlobalSyle Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import GlobalStyle from './GlobalStyle';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    min-height: 500vh;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit
+  }
+
+  button {
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
